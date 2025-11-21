548095a85daaf028aeda17e99bc0221776cf0f10
Yaris van Thiel <yarisvanthiel@gmail.com>
2025-11-21 13:49:13 +0100

    Upgrade to spring boot 4

--- src/main/java/nl/uym/UYM.java
+++ src/main/java/nl/uym/UYM.java
@@ -2,15 +2,13 @@ package nl.uym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.WebApplicationInitializer;
 
 /**
  * @author Yaris van Thiel
  */
-@EnableCaching
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class UYM extends SpringBootServletInitializer implements WebApplicationInitializer {
 
